c1cc41a075135aa805394fe451f31aacdbb40c26
Kevin <kevindkevdan@gmail.com>
2023-02-20 17:57:00 -0500

    added update and delete for txn and user

--- user/user.go
+++ user/user.go
@@ -12,6 +12,10 @@ import (
 
 type ApiService interface {
 	MakeUser(w http.ResponseWriter, r *http.Request)
+	GetAllUsers(w http.ResponseWriter, r *http.Request)
+	GetUser(w http.ResponseWriter, r *http.Request)
+	UpdateUser(w http.ResponseWriter, r *http.Request)
+	DeleteUser(w http.ResponseWriter, r *http.Request)
 }
 
 type user struct {
@@ -44,7 +48,7 @@ func (u *user) MakeUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (u *user) GetUsers(w http.ResponseWriter, r *http.Request) {
+func (u *user) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	var users []types.User
 
 	u.db.Find(&users)
@@ -83,6 +87,76 @@ func (u *user) GetUser(w http.ResponseWriter, r *http.Request) {
 	usr.Transactions = transactions
 
 	err = json.NewEncoder(w).Encode(&usr)
+	if err != nil {
+		return
+	}
+}
+
+func (u *user) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var (
+		newUser types.User
+		oldUser types.User
+	)
+
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		log.Println("error occurred decoding", err)
+		return
+	}
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Println("id is missing in GetUser")
+		log.Println(err)
+		w.WriteHeader(404)
+	}
+
+	resp := u.db.First(&oldUser, id)
+	if resp.Error != nil {
+		log.Println(resp.Error)
+		w.WriteHeader(500)
+	}
+
+	resp = u.db.Model(&oldUser).Updates(newUser)
+	if resp.Error != nil {
+		log.Println("error occurred creating", resp.Error)
+	}
+
+	err = json.NewEncoder(w).Encode(&newUser)
+	if err != nil {
+		return
+	}
+
+}
+
+func (u *user) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	var userToDelete types.User
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Println("id is missing in GetUser")
+		log.Println(err)
+		w.WriteHeader(404)
+		return
+	}
+
+	resp := u.db.First(&userToDelete, id)
+	if resp.Error != nil {
+		log.Println(resp.Error)
+		w.WriteHeader(500)
+		return
+	}
+
+	resp = u.db.Delete(&userToDelete)
+	if resp.Error != nil {
+		log.Println(resp.Error)
+		w.WriteHeader(500)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(&userToDelete)
 	if err != nil {
 		return
 	}
